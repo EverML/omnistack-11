de2549147a073160bc2f1f33b09272ff16668843
Ever Leiva <everleiva21@gmail.com>
2020-03-29 17:31:32 -0400

    finalizado dia 2

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+//antes de todas las requisiciones, express convertira el body de lo que reciba a json
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+/*
+ * Primer parametro es el recurso, i.e /users 
+ * 
+*/ 
+
+
+/**
+ * Tipos de Parametros:
+ * 
+ * Query Params: Parametros nombrados enviados en la ruta despues del simbolo de interrogacion, 
+ * commons use cases: filtros, paginacion, etc.
+ * 
+ * 
+ * Route Params: Parametros utilizados para identificar recursos 
+ * 
+ * Request body: 
+ */
+
+
+/**
+ * SQL:
+ * NoSQL:
+*/
+
+
+
+/**
+ * Driver: SELECT * FROM users;
+ * 
+ * Query Builder: table('users')
+ * 
+ */
+
+
+
+app.listen(3333);
